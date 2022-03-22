0a8dab459d18c057fe41177aebdc274b012f63c4
farzin89 <farzin_ahmadi90@yahoo.com>
2022-03-22 17:30:05 +0100

    Drawing on images

--- 3-image and basic with openCV n/main.py
+++ 3-image and basic with openCV n/main.py
@@ -1,9 +1,20 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+img = cv2.imread('farzin.jpg.jpg')
+cv2.imshow("farzin",img)
+cv2.rectangle(img,pt1=(760,100)# top left corner
+                 ,pt2=(450,500) # buttem right corner
+                  ,color=(0,255,0)
+                  ,thickness=10)
+cv2.imshow("farzin",img)
+cv2.circle(img,center=(100,100),radius=50,color=(255,0,0),thickness=8)
+cv2.imshow("farzin",img)
+cv2.circle(img,center=(1000,700),radius=50,color=(255,0,0),thickness=-1)
+cv2.imshow("farzin",img)
+cv2.line(img,pt1=(0,0),pt2=(512,512),color=(102,255,255),thickness=5)
+cv2.imshow("farzin",img)
 
-blank_image = np.zeros(shape=(512,512,3),dtype=np.int16)
-
-print(blank_image.shape)
+cv2.waitKey(0)
 
 
