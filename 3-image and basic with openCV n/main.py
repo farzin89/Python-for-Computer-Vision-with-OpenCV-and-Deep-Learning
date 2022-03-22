597d5c65f4ee0e1c5c5cc2c52235db5995d1f33b
farzin89 <farzin_ahmadi90@yahoo.com>
2022-03-22 22:12:37 +0100

    Drawing on image part 2

--- 3-image and basic with openCV n/main.py
+++ 3-image and basic with openCV n/main.py
@@ -15,6 +15,33 @@ cv2.imshow("farzin",img)
 cv2.line(img,pt1=(0,0),pt2=(512,512),color=(102,255,255),thickness=5)
 cv2.imshow("farzin",img)
 
+# writing something on image
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,text='farzin',org=(700,300),fontFace=font,fontScale=2,color=(255,255,255),thickness=3,lineType=cv2.LINE_AA)
+cv2.imshow("farzin",img)
+
+cap = cv2.imread('cap.JPG')
+
+#percent by which the image is resized
+scale_percent = 20
+#calculate the 50 percent of original dimensions
+width = int(cap.shape[1] * scale_percent / 100)
+height = int(cap.shape[0] * scale_percent / 100)
+# dsize
+dsize = (width, height)
+
+cap_resize = cv2.resize(cap,dsize=dsize)
+
+cv2.imshow('cap',cap_resize)
+
+vertices = np.array([ [100,300],[200,200],[400,300] ,[200,400] ],dtype=np.int32)
+print(vertices)
+print(vertices.shape)
+pts = vertices.reshape((-1,1,2))
+print(pts)
+cv2.polylines(cap_resize,[pts],isClosed=True,color=(255,0,0),thickness=5)
+cv2.imshow('cap',cap_resize)
+
 cv2.waitKey(0)
 
 
