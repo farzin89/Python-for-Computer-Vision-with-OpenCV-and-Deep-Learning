5ae539373adb8fcb8aaf1b10121f1d2ed16d46c7
farzin89 <farzin_ahmadi90@yahoo.com>
2022-03-30 16:37:15 +0200

    open camera and save video

---
+++ pythonProject/open camera and save video.py
@@ -0,0 +1,32 @@
+
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+width =int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height =int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# in order to save video
+# Note: for windows -- *'DIVX'
+# for MACOS or LINUX *'XVID'
+writer = cv2.VideoWriter('mysupervideo.mp4',cv2.VideoWriter_fourcc(*'DIVX'),20,(width,height))
+
+while True:
+
+    ret,frame = cap.read()
+
+    # operation (drawing)
+    writer.write(frame)
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("frame",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
+
+
